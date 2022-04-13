f5a737f29e1e3a9052d6eb2bef284d5564c9521d
Warno0 <warno38@gmail.com>
2022-04-13 18:41:16 +0800

    bruteforced the wait. sorry

--- bin/index.js
+++ bin/index.js
@@ -6,22 +6,25 @@ const { ethers } = require("ethers");
 const yargs = require("yargs");
 require('dotenv').config()
 
-const api = process.env.API_KEY;
-const provider = new ethers.providers.EtherscanProvider("rinkeby", api);
+const KEY = process.env.API_KEY;
+const provider = new ethers.providers.EtherscanProvider("rinkeby", KEY);
 
-/*const options = yargs
+const options = yargs
  .usage("Usage: -f <from> -t <to> -a <amount>  (Wallets IDs must be written in this format: #1, #2)")
  .option("f", { alias: "from", describe: "ID of the source for the transfer", type: "string", demandOption: true })
  .option("t", { alias: "to", describe: "ID of the recipient for the transfer", type: "string", demandOption: true })
  .option("a", { alias: "amount", describe: "Amount of Eth to be transfered", type: "string", demandOption: true })
- .argv;*/
+ .argv;
 
-//var fromID = options.from.split('#')[1];
-//var toID = options.to.split('#')[1];
+var fromID = options.from.split('#')[1] - 1;
+var toID = options.to.split('#')[1] - 1;
+
+var Wallets = []
+var signingWallet;
+var tx;
 
 
 let importWallets = () => {
-  let Wallets = [];
   fs.createReadStream('Wallets.csv')
     .pipe(csv())
     .on('data', (row) => {
@@ -32,12 +35,13 @@ let importWallets = () => {
     })
 }
 
-var Wallets = importWallets();
-
-var signingWallet = new ethers.Wallet(/*Wallets[fromID].PrivateKey*/"0x57aa4ab356f829d7ff36cfe76cde84787d1a314d26b5fca3eb34b80c08e83d9e", provider);
-var tx = {
-  to: /*Wallets[toID].Address*/"0x58D9f3d384e8dD0B88452450A549C0d1A7b78Eb8",
-  value: ethers.utils.parseEther(/*options.amount*/"1")
+let app = () => {
+  signingWallet = new ethers.Wallet(Wallets[fromID].PrivateKey, provider);
+  let address = Wallets[toID].Address;
+  tx = {
+    to: address,
+    value: ethers.utils.parseEther(options.amount)
+  }
 }
 
 let transaction = async () => {
@@ -48,3 +52,9 @@ let transaction = async () => {
     console.log(error);
   }
 }
+
+importWallets()
+setTimeout(() => {
+  app();
+  transaction()
+}, 1000);
